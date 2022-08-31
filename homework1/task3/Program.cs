9818b22453d6486a62d0db7f4aea97513c9eedad
NikitaZharikov <nikitkazharikov@gmail.com>
2022-09-01 00:13:49 +0300

    task3

--- homework1/task3/Program.cs
+++ homework1/task3/Program.cs
@@ -1,1 +1,11 @@
-﻿Console.WriteLine("Hello, World!");
+﻿Console.Write("Введите число ");
+int num = Convert.ToInt32(Console.ReadLine());
+int result = num % 2;
+if (result == 0)
+{
+    Console.Write("Число " + num + " четное");
+}
+else
+{
+    Console.Write("Число " + num + " нечетное");
+};
