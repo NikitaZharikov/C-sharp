15950dc3c518abc12d2772d1466308a9dc00152f
NikitaZharikov <nikitkazharikov@gmail.com>
2022-08-31 23:39:13 +0300

    task1

---
+++ homework1/task1/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите первое число ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число ");
+int b = Convert.ToInt32(Console.ReadLine());
+if (a > b)
+{
+    Console.Write("Число a больше чила b");
+}
+else
+{
+    Console.Write("Число b больше числа a");
+};
