6ab0ff02fc262234bb55c2fb2dab603e99e550cb
NikitaZharikov <nikitkazharikov@gmail.com>
2022-09-07 14:12:20 +0300

    complete task1

--- homework3/task1/Program.cs
+++ homework3/task1/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int oldValue = number;
+int newValue = 0;
+int dig = 0;
+
+while (number > 0)
+{
+    dig = number % 10;
+    newValue = newValue * 10 + dig;
+    number = number / 10;
+}
+
+if (newValue == oldValue)
+    Console.WriteLine("Число является палиндромом");
+
+else
+    Console.WriteLine("Число не является палиндромом");
