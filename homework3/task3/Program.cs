0c39cb44ceb248fa1a1a32d4a44fae719dd7c2e0
NikitaZharikov <nikitkazharikov@gmail.com>
2022-09-07 16:44:12 +0300

    Complete task3

--- homework3/task3/Program.cs
+++ homework3/task3/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string numCubes(int num)
+{
+    int i = 2;
+    string cubes = "1";
+    while (i <= num)
+    {
+        cubes = cubes + "," + Convert.ToString(i * i * i);
+        i++;
+    }
+    return cubes;
+}
+
+Console.WriteLine("Введите число");
+int num = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(numCubes(num));
